277a2e81fd11e53dc96e9b2356941f21ac1676f9
Navleen-Saini <navanmol777@gmail.com>
2026-01-20 15:13:27 -0600

    Initial commit

---
+++ credentials.js
@@ -0,0 +1,14 @@
+//Name : Navleen Saini
+//StudID : 3176358
+
+module.exports = {
+    mongoUri: "mongodb+srv://navanmol777_db_user:mzWVe1wrnl5xT6Eh@ass03-advanceip-cluster.tatwegy.mongodb.net/MyUsers?retryWrites=true&w=majority"
+};
+
+module.exports = {
+    MONGODB: {
+        user: "navanmol777_db_user",
+        login: "mzWVe1wrnl5xT6Eh",
+        cluster: "ass03-advanceip-cluster.tatwegy.mongodb.net/MyUsers"
+    }
+};
---
+++ models/ContactModels.js
@@ -0,0 +1,26 @@
+//Name: Navleen Saini
+//StudID: 3176358
+
+const mongoose = require('mongoose');
+
+const UserSchema = new mongoose.Schema({
+    name: { type: String, required: true, trim: true, minlength: 2 },
+    email: { 
+        type: String, 
+        required: true, 
+        unique: true, 
+        lowercase: true, 
+        trim: true, 
+        match: /^[^\s@]+@[^\s@]+\.[^\s@]+$/ 
+    },
+    password: { type: String, required: true },
+    planId: { type: String, enum: ["basic", "gold", "platinum"], required: true },
+    currency: { 
+        type: String,
+        required: true,
+        default: "CAD" },
+    planPrice: {type: Number, required: true, min: 0},
+    createdAt: { type: Date, default: Date.now }
+});
+
+module.exports = mongoose.model("User", UserSchema);
